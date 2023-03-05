70c0cf93a72fe4f1373bdc3c2ac8687cc3906126
honorific <honorific.info@gmail.com>
2023-03-05 10:24:21 +0330

    fix snackbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Loading from './components/Loading'
 import NavBar from './components/NavBar'
 import Login from './components/user/Login'
+import Notification from './components/user/Notification'
 
 function App() {
   return (
     <>
+      <Loading />
+      <Notification />
       <Login />
       <NavBar />
     </>
