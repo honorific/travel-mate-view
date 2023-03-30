fe987a18489b15cdb1833bb760454e2195e8d47c
honorific <honorific.info@gmail.com>
2023-03-30 13:28:52 +0430

    create room action

--- src/actions/room.js
+++ src/actions/room.js
@@ -1,4 +1,5 @@
 import {useValue} from '../context/ContextProvider'
+import fetchData from '../utils/fetchData'
 
 const url = `${process.env.REACT_APP_SERVER_URL}/room`
 const {
@@ -6,4 +7,23 @@ const {
   dispatch,
 } = useValue()
 
-export const createRoom = async (room, currentUser, dispatch) => {}
+export const createRoom = async (room, currentUser, dispatch) => {
+  dispatch({type: 'START_LOADING'})
+  const result = await fetchData({
+    url,
+    body: room,
+    token: currentUser?.token,
+    dispatch,
+  })
+  if (result) {
+    dispatch({
+      type: 'UPDATE_ALERT',
+      payload: {
+        open: true,
+        severity: 'success',
+        message: 'the room created successfully',
+      },
+    })
+  }
+  dispatch({type: 'END_LOADING'})
+}
