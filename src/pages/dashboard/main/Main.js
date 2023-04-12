c92a5119996b1d13b4f08913a48d047964168ab5
honorific <honorific.info@gmail.com>
2023-04-12 11:10:57 +0430

    total users section in main page of dashboard

--- src/pages/dashboard/main/Main.js
+++ src/pages/dashboard/main/Main.js
@@ -1,3 +1,5 @@
+import {Group} from '@mui/icons-material'
+import {Box, Paper, Typography} from '@mui/material'
 import {useEffect} from 'react'
 
 function Main({setSelectedLink, link}) {
@@ -5,7 +7,31 @@ function Main({setSelectedLink, link}) {
     setSelectedLink(link)
   }, [])
 
-  return <div>Main</div>
+  return (
+    <Box
+      sx={{
+        display: {
+          xs: 'flex',
+          md: 'grid',
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          gridAutoRows: 'minmax(100px,auto)',
+          gap: 3,
+          textAlign: 'center',
+          flexDirection: 'column',
+        },
+      }}
+    >
+      <Paper elevation={3} sx={{p: 3}}>
+        <Typography variant='h4'>Total users</Typography>
+        <Box
+          sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}
+        >
+          <Group sx={{height: 100, width: 100, opacity: 0.3, mr: 1}} />
+          <Typography variant='h4'>10</Typography>
+        </Box>
+      </Paper>
+    </Box>
+  )
 }
 
 export default Main
