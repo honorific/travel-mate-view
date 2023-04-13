69635d89c563250fbcce43c9bf89d10783f6fd59
honorific <honorific.info@gmail.com>
2023-04-13 18:36:26 +0430

    add mui x data grid to package.json

--- src/pages/dashboard/users/Users.js
+++ src/pages/dashboard/users/Users.js
@@ -1,10 +1,21 @@
+import {Box, Typography} from '@mui/material'
 import {useEffect} from 'react'
 
 function Users({setSelectedLink, link}) {
   useEffect(() => {
     setSelectedLink(link)
   }, [])
-  return <div>Users</div>
+  return (
+    <Box sx={{height: 400, width: '100%'}}>
+      <Typography
+        variant='h3'
+        component='h3'
+        sx={{textAlign: 'center', mt: 3, mb: 3}}
+      >
+        Manage users
+      </Typography>
+    </Box>
+  )
 }
 
 export default Users
