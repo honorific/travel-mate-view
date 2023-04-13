cc3b48caaab62765a98f17e7f2cb5267caf0b55b
honorific <honorific.info@gmail.com>
2023-04-13 13:51:48 +0430

    fix two digit for month

--- src/pages/dashboard/main/AreaRoomsUsers.js
+++ src/pages/dashboard/main/AreaRoomsUsers.js
@@ -8,57 +8,28 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-]
-
+const months = 5
+const today = new Date()
+const tempData = []
+for (let i = 0; i < months; i++) {
+  const date = new Date(
+    today.getFullYear(),
+    today.getMonth() - (months - (i + 1)),
+  )
+  tempData.push({
+    date,
+    name: date.getFullYear() + '/' + ('0' + (date.getMonth() + 1)).slice(-2),
+    rooms: 0,
+    users: 0,
+  })
+}
+console.log('tempdata is: ', tempData)
 const AreaRoomsUsers = () => {
   return (
     <div style={{width: '100%', height: 300, minWidth: 250}}>
       <ResponsiveContainer>
         <AreaChart
-          data={data}
+          data={tempData}
           margin={{
             top: 10,
             right: 10,
@@ -83,13 +54,6 @@ const AreaRoomsUsers = () => {
             stackId='1'
             stroke='#82ca9d'
             fill='#82ca9d'
-          />
-          <Area
-            type='monotone'
-            dataKey='amt'
-            stackId='1'
-            stroke='#ffc658'
-            fill='#ffc658'
           />
         </AreaChart>
       </ResponsiveContainer>
