74fd8698dd2eca500ad6b0e5ccad2410a6bd2817
honorific <honorific.info@gmail.com>
2023-04-13 19:05:12 +0430

    fix avatar in data grid

--- src/pages/dashboard/users/Users.js
+++ src/pages/dashboard/users/Users.js
@@ -1,10 +1,37 @@
-import {Box, Typography} from '@mui/material'
-import {useEffect} from 'react'
+import {Avatar, Box, Typography} from '@mui/material'
+import {DataGrid} from '@mui/x-data-grid'
+import {useEffect, useMemo} from 'react'
+import {useValue} from '../../../context/ContextProvider'
+import {getUsers} from '../../../actions/user'
 
 function Users({setSelectedLink, link}) {
+  const {
+    state: {users},
+    dispatch,
+  } = useValue()
+
   useEffect(() => {
     setSelectedLink(link)
+    if (users.length === 0) getUsers(dispatch)
   }, [])
+
+  const columns = useMemo(
+    () => [
+      {
+        field: 'photoURL',
+        headerName: 'Avatar',
+        width: 60,
+        renderCell: (params) => <Avatar src={params.row.photoURL} />,
+      },
+      {field: 'name', headerName: 'Name', width: 170},
+      {field: 'email', headerName: 'Email', width: 200},
+      {field: 'role', headerName: 'Role', width: 100},
+      {field: 'active', headerName: 'Active', width: 100},
+      {field: 'createdAt', headerName: 'Created at', width: 200},
+      {field: '_id', headerName: 'ID', width: 220},
+    ],
+    [],
+  )
   return (
     <Box sx={{height: 400, width: '100%'}}>
       <Typography
@@ -14,6 +41,7 @@ function Users({setSelectedLink, link}) {
       >
         Manage users
       </Typography>
+      <DataGrid columns={columns} rows={users} getRowId={(row) => row._id} />
     </Box>
   )
 }
