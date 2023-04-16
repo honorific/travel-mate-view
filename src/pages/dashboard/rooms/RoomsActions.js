e36e7b8b91182335357892b55062e20aa06ac9c5
honorific <honorific.info@gmail.com>
2023-04-16 16:02:19 +0430

    add icons to action section of table

--- src/pages/dashboard/rooms/RoomsActions.js
+++ src/pages/dashboard/rooms/RoomsActions.js
@@ -1,7 +1,27 @@
+import {Delete, Edit, Preview} from '@mui/icons-material'
+import {Box, IconButton, Tooltip} from '@mui/material'
 import React from 'react'
 
-const RoomsActions = () => {
-  return <div>RoomsActions</div>
+const RoomsActions = ({params}) => {
+  return (
+    <Box>
+      <Tooltip title='view room details'>
+        <IconButton onClick={() => {}}>
+          <Preview />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='edit this room'>
+        <IconButton onClick={() => {}}>
+          <Edit />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title='delete this room'>
+        <IconButton onClick={() => {}}>
+          <Delete />
+        </IconButton>
+      </Tooltip>
+    </Box>
+  )
 }
 
 export default RoomsActions
