20c3f50a5600ce5498f8dc742752956ce5042d6f
honorific <honorific.info@gmail.com>
2023-04-16 16:19:51 +0430

    show room info when click on view icon

--- src/pages/dashboard/rooms/RoomsActions.js
+++ src/pages/dashboard/rooms/RoomsActions.js
@@ -1,12 +1,19 @@
 import {Delete, Edit, Preview} from '@mui/icons-material'
 import {Box, IconButton, Tooltip} from '@mui/material'
 import React from 'react'
+import {useValue} from '../../../context/ContextProvider'
 
 const RoomsActions = ({params}) => {
+  const {dispatch} = useValue()
   return (
     <Box>
       <Tooltip title='view room details'>
-        <IconButton onClick={() => {}}>
+        <IconButton
+          onClick={() => {
+            dispatch({type: 'UPDATE_ROOM', payload: params.row})
+            console.log('i am clicked')
+          }}
+        >
           <Preview />
         </IconButton>
       </Tooltip>
