0ba6d8d39bf7ad26fbe524d84d5b963ee2da723b
honorific <honorific.info@gmail.com>
2023-04-17 12:05:44 +0430

    deleing images of deleted room

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -97,6 +97,12 @@ const reducer = (state, action) => {
     case 'UPDATE_USERS':
       return {...state, users: action.payload}
 
+    case 'DELETE_ROOM':
+      return {
+        ...state,
+        rooms: state.rooms.filter((room) => room._id !== action.payload),
+      }
+
     default:
       throw new Error("you didn't pass a proper action")
   }
