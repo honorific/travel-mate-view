a48d42ebe3b2f6fb981bc1f60aa0fc1939d6e770
honorific <honorific.info@gmail.com>
2023-04-21 11:59:08 +0430

    implementation of remove info from localStoraage

--- src/hooks/useCheckToken.js
+++ src/hooks/useCheckToken.js
@@ -1,10 +1,20 @@
 import {useEffect} from 'react'
 import {useValue} from '../context/ContextProvider'
 import jwtDecode from 'jwt-decode'
+import { storeRoom } from '../actions/room'
+import { logOut } from '../actions/user'
 
 const useCheckToken = () => {
   const {
-    state: {currentUser},
+    state: {
+      currentUser,
+      location,
+      details,
+      images,
+      updatedRoom,
+      deletedImages,
+      addedImages,
+    },
     dispatch,
   } = useValue()
 
@@ -12,7 +22,16 @@ const useCheckToken = () => {
     if (currentUser) {
       const decodedToken = jwtDecode(currentUser.token)
       if (decodedToken.exp * 1000 < new Date().getTime()) {
-        dispatch({type: 'UPDATE_USER', payload: null})
+        storeRoom(
+          location,
+          details,
+          images,
+          updatedRoom,
+          deletedImages,
+          addedImages,
+          currentUser.id,
+        )
+        logOut(dispatch)
       }
     }
   }, [])
